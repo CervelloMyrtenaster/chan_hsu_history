23932405c6ffbcfec75cc3e3b2c65fc523d295ac
CervelloMyrtenaster <ericapple3000@yahoo.com.tw>
2025-09-21 00:59:33 +0800

    Add index.html and data.js

--- data.js
+++ data.js
@@ -3,8 +3,15 @@ const records = {
   
   "8": {
     "4": [
-      { type: "link", content: "https://photos.app.goo.gl/nsukrSJaPRh8LTmD8", label: "2016年8月4日 等著被砸店" },
-      { type: "link", content: "https://photos.app.goo.gl/xTMABD1L8PYto82y7", label: "2016年8月4日 我會先剁你手" }
+      //{ type: "text", content: "測試文字" },
+      { type: "link", content: "https://photos.app.goo.gl/nsukrSJaPRh8LTmD8", label: "2016年8月4日 等著上社會新聞頭版 家裡開中藥店 等著被砸店" },
+      { type: "link", content: "https://photos.app.goo.gl/xTMABD1L8PYto82y7", label: "2016年8月4日 我會先剁你手 不要懷疑" }
+    ]
+  }
+,
+  "11": {
+    "11": [
+      { type: "link", content: "https://photos.app.goo.gl/5uxQhntJZdxREUah7", label: "2016年11月11日 夠綠了還抽綠菸" }
     ]
   }
 
