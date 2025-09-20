957d7eef4de2ce828bcea94a79b5b1283e368f4d
CervelloMyrtenaster <ericapple3000@yahoo.com.tw>
2025-09-20 23:29:05 +0800

    Add index.html and data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+// 展旭紀錄資料庫
+const records = {
+  
+  "8": {
+    "4": [
+      { type: "link", content: "https://photos.app.goo.gl/nsukrSJaPRh8LTmD8", label: "2016年8月4日 等著被砸店" },
+      { type: "link", content: "https://photos.app.goo.gl/xTMABD1L8PYto82y7", label: "2016年8月4日 我會先剁你手" }
+    ]
+  }
+
+};
